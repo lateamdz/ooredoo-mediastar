48f705db4a88040f83a6b3877179eaebcc5e43b6
samikhammar <sami.khammar.dev@gmail.com>
2023-04-24 22:30:49 +0100

    fixes 02

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,4 +1,15 @@
-       
+let openSupportMenu=false;
+function openSupport(){
+    console.log("supprt clicked")
+    if(openSupportMenu){
+        document.getElementById("support-menu").classList.remove("expanded")
+        document.getElementById("support-btn").classList.remove("expanded")
+    }else{
+        document.getElementById("support-menu").classList.add("expanded")
+        document.getElementById("support-btn").classList.add("expanded")
+    }
+    openSupportMenu=!openSupportMenu;
+}
             
 
 
